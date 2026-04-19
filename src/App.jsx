46c7fffa9698b53a13a072eca3facf0ba61703ec
= <Sanidhi2207@gmail.com>
2026-04-19 20:01:03 +0530

    feat : add loading and error handling with improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,13 @@ function App() {
   const [song,setSong]=useState("");
   const [artist,setArtist]=useState("");
   const [lyrics,setLyrics]=useState("");
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState("")
 
  const handleSearch = async () => {
+  setLoading(true)
+  setError("");
+  setLyrics("");
   try {
     const response = await fetch(
       `https://api.lyrics.ovh/v1/${artist}/${song}`
@@ -15,25 +20,39 @@ function App() {
 
     const data = await response.json();
 
-    setLyrics(data.lyrics);
+    if(data.lyrics){
+      setLyrics(data.lyrics);
+    }else {
+      setError("Lyrics not found")
+    }
+   
   } catch (error) {
-    console.log(error);
+    setError("Something went wrong ❌")
   }
+  setLoading(false)
 };
+
+
   return (
-    <div>
+    <div className="container">
       <h1>Lyrics finder</h1>
+      <div className='search-box'>
       <input type="text" placeholder='Enter song name' value={song} onChange={(e)=> setSong(e.target.value)}/>
       <br/>
       <input type="text" placeholder='Enter artist name' value={artist} onChange={(e)=>setArtist(e.target.value)}/>
       <br/>
-      <button onClick={handleSearch}>Search</button>
-     <div>
-       <h3>Lyrics:</h3>
+      <button onClick={handleSearch} className='search-box' disabled={loading}>{loading ? "Searching..." : "Search"}</button>
+      </div>
+      {loading && <p className='loading'>Loading...⏳</p>}
+     {error && <p className='error'>{error}</p>}
+     {lyrics && (
+     <div className='lyrics-box'>
+      <h3>Lyrics:</h3>
        <p>{lyrics}</p>
      </div>
+    )}
     </div>
   )
 }
 
-export default App
+export default App;
