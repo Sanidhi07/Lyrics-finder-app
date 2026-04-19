5e9aeee26e580da6fae5ab593a5abe0949538f73
= <Sanidhi2207@gmail.com>
2026-04-19 20:36:43 +0530

    feat: add enter key search and copy lyrics functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ function App() {
   const [lyrics,setLyrics]=useState("");
   const [loading,setLoading]=useState(false);
   const [error,setError]=useState("")
+  const [copied, setCopied] = useState(false);
+
+const handleCopy = () => {
+  navigator.clipboard.writeText(lyrics);
+  setCopied(true);
+  setTimeout(() => setCopied(false), 1500);
+};
+  
 
  const handleSearch = async () => {
   setLoading(true)
@@ -30,24 +38,32 @@ function App() {
     setError("Something went wrong ❌")
   }
   setLoading(false)
-};
+
+ };
+
+
 
 
   return (
     <div className="container">
       <h1>Lyrics finder</h1>
       <div className='search-box'>
-      <input type="text" placeholder='Enter song name' value={song} onChange={(e)=> setSong(e.target.value)}/>
+      <input type="text" placeholder='Enter song name' value={song} onChange={(e)=> setSong(e.target.value)} onKeyDown={(e) => e.key === "Enter" && handleSearch()}/>
       <br/>
-      <input type="text" placeholder='Enter artist name' value={artist} onChange={(e)=>setArtist(e.target.value)}/>
+      <input type="text" placeholder='Enter artist name' value={artist} onChange={(e)=>setArtist(e.target.value)}  onKeyDown={(e) => e.key === "Enter" && handleSearch()}/>
       <br/>
-      <button onClick={handleSearch} className='search-box' disabled={loading}>{loading ? "Searching..." : "Search"}</button>
+      <button onClick={handleSearch} disabled={loading}>{loading ? "Searching..." : "Search"}</button>
       </div>
       {loading && <p className='loading'>Loading...⏳</p>}
      {error && <p className='error'>{error}</p>}
      {lyrics && (
      <div className='lyrics-box'>
       <h3>Lyrics:</h3>
+      <button
+      onClick={handleCopy}
+    >
+       {copied ? "Copied! ✅" : "Copy Lyrics"}
+    </button>
        <p>{lyrics}</p>
      </div>
     )}
